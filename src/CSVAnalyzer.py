5b3fc14306b84ab6b46709d1e8854e67d1d68a97
Tudor Dragan <tudorgk@gmail.com>
2016-03-19 20:11:27 +0100

    sanitized data

--- src/CSVAnalyzer.py
+++ src/CSVAnalyzer.py
@@ -11,6 +11,7 @@ class CSVAnalyser:
     def __init__(self, csvfile = None):
         self.csvfile = csvfile
         self.imported_data = None
+        self.sanitized_data = []
         np.set_printoptions(threshold=np.nan)
 
     def analyze(self):
@@ -20,11 +21,19 @@ class CSVAnalyser:
         self.imported_data = np.asarray(data)
         self.remove_invalid_entries()
 
-
     def remove_invalid_entries(self):
-        print(self.imported_data)
-        self.imported_data = self.imported_data[np.all(self.imported_data != '', axis=1)]
-        print(self.imported_data)
+        f1 = open('../tmp/not_removed.txt', 'w+')
+        f2 = open('../tmp/removed.txt', 'w+')
+
+        for i in range(self.imported_data[:,].shape[0]):
+            rating = self.imported_data[i,1]
+            text = self.imported_data[i,3]
+            row = []
+            if rating == '1' or rating == '-1' or rating == '0':
+                row.append(int(rating))
+                row.append(text)
+                self.sanitized_data.append(row)
+        f2.write(str(self.sanitized_data))
 
 
 
