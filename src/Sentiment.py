414f63b72900e4796414d0a447f38e8989644997
Tudor Dragan <tudorgk@gmail.com>
2016-03-21 20:27:48 +0100

    constructing the arrays

--- src/Sentiment.py
+++ src/Sentiment.py
@@ -3,6 +3,8 @@ import sys
 import glob
 import argparse
 import json
+from io import StringIO
+import ast
 import pycurl
 import numpy as np
 import re
@@ -13,12 +15,17 @@ class SentimentV1:
     def __init__(self, data = None):
         self.data = data
         self.results = []
+        self.downloaded_results = []
+        self.sentimentV1_data = []
+        self.index_to_remove = []
 
-    def run_analysis(self):
+
+    def download_sentiments(self):
+        print("Download sentiment data from webservice")
         f1 = open('../tmp/results.txt', 'w+')
 
-        for i in range(self.data[:,].shape[0]):
-            print(i)
+        for i in range(self.data[:, ].shape[0]):
+            print("Downloading entry" + i)
             escaped = re.escape(self.data[i, 1].encode('utf-8'))
 
             post_data = {'text': escaped}
@@ -39,6 +46,45 @@ class SentimentV1:
             self.results.append(response)
 
         f1.write(str(self.results))
+
+    def import_mined_json(self):
+        print("Import json data")
+        f1 = open('../tmp/results.txt', 'r')
+        result_string = f1.read()
+        self.downloaded_results = ast.literal_eval(result_string)
+        f1.close()
+
+    def construct_arrays(self):
+        print("Constructing arrays")
+        print(self.data.shape)
+        print(len(self.downloaded_results))
+        for i in range(self.data[:, ].shape[0]):
+            # using try-catch because some downloaded results don't have data associated because of some kind of form
+            # error. (Form Validation Errors text: This field is required.)
+            try:
+                json_row = json.loads(self.downloaded_results[i])
+                if json_row['label'] == "pos":
+                    raw_value = 1
+                elif json_row['label'] == "neg":
+                    raw_value = -1
+                else:
+                    raw_value = 0
+
+                row = [self.data[i, 0],raw_value]
+                self.sentimentV1_data.append(row)
+
+            except:
+                # save the indices that we need to remove for the second sentiment mining
+                self.index_to_remove.append(i)
+                pass
+
+        print(self.sentimentV1_data)
+
+    def run_analysis(self):
+        print("Run analysis")
+        #self.download_sentiments()
+        self.import_mined_json()
+        self.construct_arrays()
 
 
 
